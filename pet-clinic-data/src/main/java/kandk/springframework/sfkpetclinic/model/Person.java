becae064e91c05db0179f353b87556fd65708ab8
kithy1 <kithy@interia.eu>
2020-05-20 14:14:22 +0200

    Adding BaseEntity. closes #10

--- pet-clinic-data/src/main/java/kandk/springframework/sfkpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/kandk/springframework/sfkpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package kandk.springframework.sfkpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/kandk/springframework/sfkpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/kandk/springframework/sfkpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package kandk.springframework.sfkpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
