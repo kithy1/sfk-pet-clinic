c41d4b30bcd5c985e81e957661903ec37d459c0f
kithy1 <kithy@interia.eu>
2020-05-22 13:26:36 +0200

    Impl Map based Services. closes #6

---
+++ pet-clinic-data/src/main/java/kandk/springframework/sfkpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package kandk.springframework.sfkpetclinic.services.map;
+
+import kandk.springframework.sfkpetclinic.model.Pet;
+import kandk.springframework.sfkpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/kandk/springframework/sfkpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package kandk.springframework.sfkpetclinic.services.map;
+
+import kandk.springframework.sfkpetclinic.model.Vet;
+import kandk.springframework.sfkpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
