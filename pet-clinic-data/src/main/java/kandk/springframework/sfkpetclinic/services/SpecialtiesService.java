d700c4a1192dadcc6680a08530a75e8adc97da84
kithy1 <kithy@interia.eu>
2020-05-25 18:21:15 +0200

    added SpecialtyMapService closes #31

---
+++ pet-clinic-data/src/main/java/kandk/springframework/sfkpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package kandk.springframework.sfkpetclinic.services;
+
+import kandk.springframework.sfkpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
