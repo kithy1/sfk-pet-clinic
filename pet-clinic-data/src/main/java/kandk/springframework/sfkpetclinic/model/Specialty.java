2c574914fe2f7a8976d1c20feb0d6365ec041e85
kithy1 <kithy@interia.eu>
2020-05-29 16:48:07 +0200

    updated vet to use JPA. closes 37

--- pet-clinic-data/src/main/java/kandk/springframework/sfkpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/kandk/springframework/sfkpetclinic/model/Specialty.java
@@ -1,5 +1,10 @@
 package kandk.springframework.sfkpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
     private String description;
