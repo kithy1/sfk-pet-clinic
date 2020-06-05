bb6d046f606225bbaee9ca6992993ef2b1c30d66
kithy1 <kithy@interia.eu>
2020-06-05 14:53:16 +0200

    added tests. closes #49

---
+++ pet-clinic-data/src/test/java/kandk/springframework/sfkpetclinic/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,86 @@
+package kandk.springframework.sfkpetclinic.services.map;
+
+import kandk.springframework.sfkpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerMapServiceTest {
+
+    OwnerMapService ownerMapService;
+    final Long ownerId = 1L;
+    final String lastName ="Smith";
+
+    @BeforeEach
+    void setUp() {
+        ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
+        ownerMapService.save(Owner.builder().id(ownerId).lastname(lastName).build());
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> ownerSet= ownerMapService.findAll();
+
+        assertEquals(1,ownerSet.size());
+    }
+
+    @Test
+    void deleteById() {
+        ownerMapService.deleteById(ownerId);
+        assertEquals(0,ownerMapService.findAll().size());
+
+    }
+
+    @Test
+    void delete() {
+        ownerMapService.delete(ownerMapService.findById(ownerId));
+        assertEquals(0,ownerMapService.findAll().size());
+    }
+
+    @Test
+    void saveExistingId() {
+        Long id = 2L;
+        Owner owner2 = Owner.builder().id(2L).build();
+        Owner savedOwner = ownerMapService.save(owner2);
+        assertEquals(id,savedOwner.getId());
+    }
+    @Test
+    void saveNoId(){
+        Owner savedOwner = ownerMapService.save(Owner.builder().build());
+
+        assertNotNull(savedOwner);
+        assertNotNull(savedOwner.getId());
+    }
+
+    @Test
+    void findById() {
+        Owner owner = ownerMapService.findById(ownerId);
+        assertEquals(ownerId,owner.getId());
+    }
+
+    @Test
+    void findByLastName() {
+        Owner smith = ownerMapService.findByLastName(lastName);
+        assertNotNull(smith);
+        assertEquals(ownerId,smith.getId());
+    }
+
+    @Test
+    void findAllByLastNameLikeReturnEmptyList() {
+        List<Owner> jenkins = ownerMapService.findAllByLastNameLike("Jenkins");
+        assertNotNull(jenkins);
+        assertEquals(0, jenkins.size());
+    }
+    @Test
+    void findAllByLastNameLikeContainRightObject(){
+        List<Owner> ownerList = ownerMapService.findAllByLastNameLike(lastName);
+        Owner owner = ownerList.get(0);
+        assertNotNull(ownerList);
+        assertTrue(ownerList.contains(owner));
+
+    }
+}
